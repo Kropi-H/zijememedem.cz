57107d686f696db6c8b78f3f56f33f0f2db54ece
Honza Kropacek <kropacek.h@gmail.com>
2019-01-29 19:46:08 +0100

    first touch

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="cz" dir="ltr">
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131382031-1"></script>
+
+    <meta charset="utf-8">
+    <title>ZIJEMEMEDEM</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/default.css">
+
+  </head>
+  <body>
+<div class="container"><!--  Start Of Container -->
+    <!-- Nav Start-->
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-light">
+  <a class="navbar-brand" href="#">
+    ZijemeMedem
+  </a>
+  <div class="">
+    <a class="navbar-nav" href="#">
+      <img src="images/zijememedem_logo.svg" width="80" height="80" class="d-inline-block menu_logo" alt="">
+    </a>
+  </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link" href="#about_us">O Nás</a>
+      <a class="nav-item nav-link" href="#produckts">Naše Produkty</a>
+      <a class="nav-item nav-link" href="#offer">Pro Vás</a>
+      <a class="nav-item nav-link" href="#contact">Kontakt</a>
+    </div>
+  </div>
+</nav>
+<!-- Nav End-->
+
+
+
+    <section id="aktual">
+        <h1>Aktuality:</h1>
+        <p>
+          Momentálně je všechen med ze sezóny 2018 vyprodán. Budete-li chtít mít jistotu, že v roce 2019 se na Vás dostane, neváhejte si med rezervovat.
+        </p>
+      </section>
+
+
+<section id="about_us">
+  <h3>O nás</h3>
+  <p>
+    Své první včelky jsem převzal po smrti svého dědy ze Smrkova v roce 1992. To mi bylo tehdy 13 let. Jednalo se o 5 včelstev v klasických „Budečákách“. V roce 1993 jsem převzal dalších  9 včelstev po svém strejdovi z Chabrovic. Tyto včelky byly už v modernějších úlech převážně typu „Český univerzál“. Tyto dva úlové systémy jsem časem převedl na nový a jednotný systém úlů Langstroth. Do roku 2017 jsem udržoval stav včelstev do 20ks.
+    Ve svých 25 letech jsem se seznámil se svou budoucí ženou Peťou a začali jsme včelařit společně. Peťa zajišťuje spíše fyzicky méně náročnou práci a já zajišťuji tu ostatní☺.
+    Na přelomu roku 2017/2018 se v nás něco zlomilo a rozhodli jsme se, že včelky rozšíříme a budeme včelařit trochu více a toho se držíme dodnes☺.
+    Současně obhospodařujeme (roku 2018) 49 včelstev na třech stanovištích. Produkujeme 4 druhy medu: květový, květoý-pastovaný, medovicový-smíšený a medovicový.
+  </p>
+
+</section>
+
+<section id="produckts">
+  <h3>Naše produkty:</h3>
+  <div>
+    MED květový je získáván z nektarů květů rostlin z jarní snůšky. Rychleji krystalizuje. Je lehce stravitelný. Barva medu bývá světlá – od žluté až po světle hnědou.
+    MED květový-pastovaný
+    MED květový-pastovaný je květový med, který prochází velmi šetrným procesem, tzv. pastováním. Během tohoto procesu se tvoří v medu velmi jemné krystalky, které dají medu krémovitou konzistenci. Vzniká tak velmi dobře roztíratelný med, jenž nestéká.
+    MED medovicový-smíšený
+    MED medovicový-smíšený je med vytáčený v době mezi medem květovým a medovicovým.   Je tmavší barvy a krystalizuje pomaleji než medy květové. Jedná se především o med z květů lípy, maliníku, ostružin, lesních bylin a medovice.
+    MED medovicový
+    MED medovicový je med, který včely vytváří ze sladké šťávy, kterou nalézají v létě na stromech jehličnatých i listnatých. Barva medu bývá spíše tmavší, v různých tónech hnědé. Krystalizace je pomalejší.
+  </div>
+</section>
+
+<section id="offer">
+<h3>Nabídka pro včelaře:</h3>
+  <div>
+
+  </div>
+  Pro všechny včelaře nabízíme naše včelí oddělky. Většinu včelstev chováme v úlech typu 2/3 Langstroth, ale vytváříme oddělky i na převažující české rámkové míře 39x24 cm, či oddělky na rámkové míře OPTIMAL 42X17.
+  Oddělky expedujeme až v plné síle na 5-ti rámcích. Nezasíláme je prostřednictvím dopravních společností, možný je pouze osobní odběr. Těm z vás, kteří mají možnost nám předem dovést své přepravní nástavky, můžeme přemístit oddělek před expedicí přímo do Vašich přepravních nástavků, aby si včelky nemusely 2x zvykat na své nové obydlí. Pro vzdálenější včelaře nabízíme oddělky v papírových krabicích určených pro převoz oddělků.
+  Včelí matky v oddělcích jsou vždy naše letošní, ověřené, kladoucí a vybrané ty nejvitálnější. Méně vitální oddělky si necháváme a ještě u nich vyměňujeme nevýkonné matky.
+  Samozřejmostí je letošní protokol s negativním vyšetření na mor včelího plodu. Pro vzdálenější zákazníky mimo Jihočeský kraj i „Veterinární osvědčení k přemístění včel mimo území kraje“.
+  Pro přesun mimo Jihočeský kraj budeme od Vás potřebovat:
+  Registrační číslo chovatele
+  Adresa/sídlo chovatele
+  Registrační číslo stanoviště
+  Katastrální území a číslo stanoviště.
+  Objednávky prosím posílejte včas, nejlépe do konce března!
+  Expedice se realizuje během měsíce června, případně začátkem července.
+</section>
+
+
+
+<section id="contact">
+  Kontakt:
+  Vláďa a Peťa Soukupovi
+  Borotín 316
+  391 35  Borotín
+  email: info@zijememedem.cz
+  tel: +420 724 294 437
+</section>
+</div><!--  End Of Container -->
+
+
+
+
+
+
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
