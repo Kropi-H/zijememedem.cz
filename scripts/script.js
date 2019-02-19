5fe9db74fbb4d60ac2d9d034ab76e062a1f9421b
Honza Kropacek <kropacek.h@gmail.com>
2019-02-19 18:37:07 +0100

    another work

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-131382031-1');
-</script>
+window.onscroll = function() {
+  growShrinkLogo();
+};
+
+function growShrinkLogo() {
+  var Logo = document.getElementById("Logo");
+  var info = document.getElementById("aktual");
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    Logo.style.width = '40px';
+    Logo.removeChild(info);
+  } else {
+    Logo.style.width = '80px';
+  }
+}
+
+console.log("it works");
