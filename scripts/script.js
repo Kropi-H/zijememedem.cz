6734cda171edfeec3ef92a94b45e02c16e07ffa7
Honza Kropacek <kropacek.h@gmail.com>
2019-02-19 21:14:26 +0100

    another stuff

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ function growShrinkLogo() {
   var info = document.getElementById("aktual");
   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
     Logo.style.width = '40px';
-    Logo.removeChild(info);
+    info.style.visibility = 'hidden';
   } else {
     Logo.style.width = '80px';
   }
